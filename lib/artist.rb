645c6c1a6166320d72ddcfa23ae3aea0176c2091
jamieberrier <berrier.jamie@gmail.com>
2018-08-15 17:35:36 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,6 +21,7 @@ class Artist
     song.artist = self
   end
 
+  #  takes in an argument of a song name, creates a new song with it and associates the song and artist
   def add_song_by_name(name)
       song = Song.new(name)
       @@songs << song
