d613d0b42f2273c5e60bceae181b049bc9c972b8
jamieberrier <berrier.jamie@gmail.com>
2018-08-15 17:43:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,7 +26,6 @@ class Artist
       song = Song.new(song_name)
       @@songs << song
       song.artist = self
-      binding.pry
   end
 
   # is a class method that returns the total number of songs associated to all existing artists
